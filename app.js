d22cb0b9c052bc577ebdc0779599f55d9abde897
tohid_d.milan@yahoo.com <tohid_d.milan@yahoo.com>
2020-10-14 06:19:31 -0600

    adds EJS components

--- app.js
+++ app.js
@@ -4,13 +4,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+let today = new Date();
+let day = "";
 app.get("/", function (req, res) {
-  let today = new Date();
-  if (today.getDay() === 0 || today.getDay() === 6) {
-    res.send("It's the weekend");
-  } else {
-    res.send("It's working day");
+  switch (today.getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("An error has occurred");
+      break;
   }
+  res.render("list", { kindOfDay: day });
 });
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
